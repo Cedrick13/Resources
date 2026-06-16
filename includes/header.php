02d28e5e11e3f3a7422571b983605a6cf66cb91d
Cedrick Embestro <cedrick13wew@gmail.com>
2026-06-16 13:31:47 +0800

    fix

--- includes/header.php
+++ includes/header.php
@@ -5,8 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title><?php echo isset($pageTitle) ? $pageTitle : "Resources"; ?></title>
         
-        <link rel="stylesheet" href="../assets/css/look.css" />
-        <link rel="icon" type="image/x-icon" href="../assets/images/icon.png" />
+        <link rel="stylesheet" href="/assets/css/look.css" />
+        <link rel="icon" type="image/x-icon" href="/assets/images/icon.png" />
     </head>
     <body id="page">
         <div class="cursor-glow"></div>
