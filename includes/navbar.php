065e711461b118ffdccfc6830bbd35f6713c2e96
Cedrick Embestro <cedrick13wew@gmail.com>
2026-06-16 13:25:11 +0800

    update

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar">
     <div class="logo">
         <a href="index.php">
-            <img src="../images/icon.png" alt="Logo" />
+            <img src="/assets/images/logo.png" alt="Logo" />
             <div class="logo-text">
                 <span class="main">Resources</span>
                 <span class="sub">Tools Hub</span>
