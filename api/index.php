6eb48dd96ca67bcbe3dc9b183ab798b70bb8562f
Cedrick Embestro <cedrick13wew@gmail.com>
2026-06-16 11:51:05 +0800

    fix error

--- api/index.php
+++ api/index.php
@@ -1,17 +1,16 @@
 <?php 
-$pageTitle = "Resources";
-include 'includes/data.php';
-include 'includes/header.php'; 
-include 'includes/navbar.php'; 
+$pageTitle = "Resources - Tools Hub";
+include '../includes/data.php';    
+include '../includes/header.php';   
+include '../includes/navbar.php';   
 
 foreach ($resources as $sectionTitle => $cards) {
     echo '<section class="section"><h2 class="section-title">' . htmlspecialchars($sectionTitle) . '</h2><div class="cards">';
     foreach ($cards as $card) {
-        // Outputting each card directly as a single compact line to short-circuit page source bloat
         echo '<div class="card ' . $card['class'] . '"><div class="overlay"><h3>' . htmlspecialchars($card['name']) . '</h3><a href="' . $card['url'] . '" target="_blank" class="btn">View</a></div></div>';
     }
     echo '</div></section>';
 }
 
-include 'includes/footer.php'; 
+include '../includes/footer.php';   
 ?>
--- includes/header.php
+++ includes/header.php
@@ -4,8 +4,9 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title><?php echo isset($pageTitle) ? $pageTitle : "Resources"; ?></title>
-        <link rel="stylesheet" href="look.css" />
-        <link rel="icon" type="image/x-icon" href="./images/icon.png" />
+        
+        <link rel="stylesheet" href="../assets/css/look.css" />
+        <link rel="icon" type="image/x-icon" href="../assets/images/icon.png" />
     </head>
     <body id="page">
         <div class="cursor-glow"></div>
